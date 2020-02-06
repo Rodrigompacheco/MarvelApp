743641a2097352ca5a384d9104c30243c78142d6
Unknown <rodrigo.rp.pacheco@gmail.com>
2020-02-05 22:39:01 -0300

    Comic, DataPackage and ThumbImage models

---
+++ MarvelApp/MarvelApp/Model/ThumbImage.swift
@@ -0,0 +1,29 @@
+//
+//  ThumbImage.swift
+//  MarvelApp
+//
+//  Created by Rodrigo Pacheco on 05/02/20.
+//  Copyright © 2020 RodrigoPacheco. All rights reserved.
+//
+
+import Foundation
+
+struct ThumbImage: Codable {
+    var path: String
+    var `extension`: String
+   
+    enum CodingKeys: String, CodingKey {
+        case path
+        case `extension`
+    }
+    
+    func fullPath() -> String {
+        return "\(path).\(`extension`)"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        path = try values.decode(String.self, forKey: .path)
+        `extension` = try values.decode(String.self, forKey: .extension)
+    }
+}
