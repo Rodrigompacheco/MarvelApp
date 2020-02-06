a06ed09b2b4bb606fc9d2b32053c9096f9c1dfbe
Unknown <rodrigo.rp.pacheco@gmail.com>
2020-02-06 17:02:21 -0300

    Implemented the coordinator

---
+++ MarvelApp/MarvelApp/Coordinator/Coordinator.swift
@@ -0,0 +1,17 @@
+//
+//  Coordinator.swift
+//  MarvelApp
+//
+//  Created by Rodrigo Pacheco on 06/02/20.
+//  Copyright © 2020 RodrigoPacheco. All rights reserved.
+//
+
+import UIKit
+
+protocol Coordinator {
+    
+    var childCoordinators: [Coordinator] { get set }
+    var navigationController: UINavigationController { get set }
+    
+    func start()
+}
---
+++ MarvelApp/MarvelApp/Coordinator/MainCoordinator.swift
@@ -0,0 +1,39 @@
+//
+//  File.swift
+//  MarvelApp
+//
+//  Created by Rodrigo Pacheco on 06/02/20.
+//  Copyright © 2020 RodrigoPacheco. All rights reserved.
+//
+
+import UIKit
+
+class MainCoordinator: Coordinator{
+
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    let marvelApiProvider: MarvelApiProvider = MarvelApiProvider()
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        self.navigationController.setNavigationBarHidden(true, animated: false)
+    }
+    
+    func start() {
+        let presenter = CharactersListPresenter(marvelApiProvider: marvelApiProvider)
+        presenter.presenterCoordinatorDelegate = self
+        let viewController = CharactersListViewController(presenter: presenter)
+//        let viewController = CharactersListViewController.instance(presenter: presenter)
+        navigationController.pushViewController(viewController, animated: false)
+    }
+}
+
+extension MainCoordinator: CharactersListPresenterCoordinatorDelegate {
+    func didSelectCharacter(character: Character) {
+//        let presenter = CharacterDetailViewModel(character: character,
+//                                          marvelApiProvider: marvelApiProvider)
+//        let viewController = CharacterDetailViewController(viewModel: viewModel)
+//        navigationController.pushViewController(viewController, animated: true)
+        print("VAI IR PRA DEITAIL VIEW CONTROLLER")
+    }
+}
