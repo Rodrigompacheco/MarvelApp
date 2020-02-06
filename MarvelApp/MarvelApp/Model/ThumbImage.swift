1048af1eee36e26f8322cc94b051bdc3df950ea5
Unknown <rodrigo.rp.pacheco@gmail.com>
2020-02-06 19:56:29 -0300

    Fixing types

--- MarvelApp/MarvelApp/Model/ThumbImage.swift
+++ MarvelApp/MarvelApp/Model/ThumbImage.swift
@@ -10,20 +10,20 @@ import Foundation
 
 struct ThumbImage: Codable {
     var path: String
-    var `extension`: String
+    var imageExtension: String
    
     enum CodingKeys: String, CodingKey {
         case path
-        case `extension`
+        case imageExtension = "extension"
     }
     
     var fullPath: String {
-        return "\(path).\(`extension`)"
+        return "\(path).\(imageExtension)"
     }
     
     init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         path = try values.decode(String.self, forKey: .path)
-        `extension` = try values.decode(String.self, forKey: .extension)
+        imageExtension = try values.decode(String.self, forKey: .imageExtension)
     }
 }
