8866e664b97e2d5abd33c417a1adb7fbe30bd533
Unknown <rodrigo.rp.pacheco@gmail.com>
2020-02-06 17:16:54 -0300

    CharacterDetailVC initial setup

---
+++ MarvelApp/MarvelApp/Features/CharactersList/View/CharacterDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CharacterDetailViewController.swift
+//  MarvelApp
+//
+//  Created by Rodrigo Pacheco on 06/02/20.
+//  Copyright © 2020 RodrigoPacheco. All rights reserved.
+//
+
+import UIKit
+
+class CharacterDetailViewController: UIViewController {
+    
+    let presenter: CharacterDetailPresenter
+    
+    init(presenter: CharacterDetailPresenter) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
