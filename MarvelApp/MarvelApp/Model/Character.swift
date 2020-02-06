8b701700857fe8142f064c47ce25ad9ceee97275
Unknown <rodrigo.rp.pacheco@gmail.com>
2020-02-05 22:38:10 -0300

    Character

---
+++ MarvelApp/MarvelApp/Model/Character.swift
@@ -0,0 +1,31 @@
+//
+//  Character.swift
+//  MarvelApp
+//
+//  Created by Rodrigo Pacheco on 05/02/20.
+//  Copyright © 2020 RodrigoPacheco. All rights reserved.
+//
+
+import Foundation
+
+struct Character: Codable {
+    let id: Int
+    let name: String
+    let description: String
+    let thumbnail: ThumbImage?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case description
+        case thumbnail
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decode(Int.self, forKey: .id)
+        name = try values.decode(String.self, forKey: .name)
+        description = try values.decode(String.self, forKey: .description)
+        thumbnail = try values.decode(Optional<ThumbImage>.self, forKey: .thumbnail)
+    }
+}
